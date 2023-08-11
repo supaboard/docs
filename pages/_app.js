8c29987955886fe4aed6e34934dcc4cb61c749d4
Ben Mann <benmann89@gmail.com>
2023-08-11 10:45:25 +0200

    og images

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 import '@/styles/globals.css'
-import {HeroPattern} from "../components/HeroPattern"
+import Head from 'next/head'
+import { HeroPattern } from "../components/HeroPattern"
 
 
 
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <HeroPattern className="h-64 w-full" />
-            <Component {...pageProps} />
-        </>
-    )
+          <Head>
+              <title>Supaboard documentation</title>
+              <meta property="og:site_name" content="Supaboard.co" />
+              <meta property="og:locale" content="en_US" />
+              <meta property="og:type" content="website" />
+              <meta property="og:url" content="https://supaboard.co" />
+              <meta property="og:title" content="Supaboard documentation" />
+              <meta property="og:description" content="Create reporting dashboards on top of Supabase with ease" />
+          </Head>
+          <HeroPattern className="h-64 w-full" />
+          <Component {...pageProps} />
+      </>
+  )
 }
